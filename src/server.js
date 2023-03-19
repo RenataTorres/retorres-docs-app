6af14338c60110a218aec4573c2e53b05784a4cb
RenataTorres <renatatads2018@gmail.com>
2023-03-19 18:32:25 -0300

    feat: add public directory

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import url from 'url';
+import path from 'path';
 
 const app = express();
-
 const port = process.env.port || 3000;
+
+//Pega todo o caminho do computador até esse arquivo
+const currentPath = url.fileURLToPath(import.meta.url);
+//Pega o caminho desse arquivo, sobe até a pasta src e depois até o diretório raiz e depois até o public
+const publicDirectory = path.join(currentPath, '../../', 'public')
+//Para o express utilize o diretório público de forma estática
+app.use(express.static(publicDirectory))
 
 app.listen(port, () => console.log(`Servidor escutando na porta ${port}`));
