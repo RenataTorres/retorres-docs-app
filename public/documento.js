2b6fe69086005537560d263aba669f1b4d2f3ae6
RenataTorres <renatatads2018@gmail.com>
2023-03-19 19:20:36 -0300

    feat: emitting events

--- public/documento.js
+++ public/documento.js
@@ -1,1 +1,7 @@
 const socket = io();
+
+const editorText = document.getElementById('editor-texto');
+
+editorText.addEventListener('keyup', () => {
+    socket.emit('editor_text', editorText.value);
+})
